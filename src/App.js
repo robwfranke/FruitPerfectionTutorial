0d25d51392536773150faffc9cca75f0a308e092
=Rob Franke <robwfranke@gmail.com>
2021-03-10 16:17:46 +0100

    "stap 2 product.js destructured en voorzien van variables"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import './App.css';
 import Product from './Product';
 
 
+import citroenen from './assets/citroenen.jpeg';
+import limoenen from "./assets/limoenen.png"
+
+
+import ijsblokjes from './assets/ijsblokjes.jpg';
+import './App.css';
+
 function App() {
     return (
-
         <main>
-
-            <Product />>
-
-
-
-
-
+            <Product
+                image={citroenen}
+                title="Citroen"
+                description="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
+            />
+            <Product
+                image={limoenen}
+                title="Limoen"
+                description="Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten (Wijnruitfamilie). Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen."
+            />
+            <Product
+                image={ijsblokjes}
+                title="IJsblokjes"
+                description="Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje. Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen."
+            />
         </main>
     );
 }
--- src/Product.js
+++ src/Product.js
@@ -2,17 +2,17 @@ import React from 'react';
 // import './App.css';
 // import App from "./App";
 
-function Product() {
+function Product({image,title, description}) {
     return (
             <article className="product">
                 <img
-                    src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg"
-                    alt="Citroenen"/>
-                <h2 className="product-name">Citroen</h2>
+                    src={image}
+                    alt={title}/>
+                <h2 className="product-name">Citroen
+                    {title}
+                </h2>
                 <p className="product-description">
-                    Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het
-                    vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de
-                    smaak van ander voedsel.
+                    {description}
                 </p>
 
             </article>
